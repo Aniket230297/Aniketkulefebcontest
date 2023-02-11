0ab51405ba4dba11daae87b9da230b8d11d4242d
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-02-11 21:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
+let arr2 = [
+  { id:'A', name: "rajesh", age: "18", marks: 80 },
+  { id:'B', name: "sameer", age: "17", marks: 70 },
+  { id:'C', name: "yash", age: "20", marks: 75 },
+];
+
 function PrintStudentswithMap() {
   //Write your code here , just console.log
 }
@@ -16,6 +22,9 @@ function PrintStudentsbyForEach() {
 
 function addData() {
   //Write your code here, just console.log
+   let student = { id: 4, name: "susan", age: "20", marks:45 }
+  arr.push(student);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
@@ -24,4 +33,6 @@ function removeFailedStudent() {
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr3= arr.concat(arr2);
+  console.log(arr3);
 }
